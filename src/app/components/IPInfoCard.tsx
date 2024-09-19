371a5da6cf91801dc327d4838e9c7f56bf014a18
Muhammad Raza <mh.raza2906@gmail.com>
2024-09-19 20:09:02 +0100

    feat: add responsiveness

--- src/app/components/IPInfoCard.tsx
+++ src/app/components/IPInfoCard.tsx
@@ -7,22 +7,24 @@ type IPInfoCardProps = {
   isp: string;
 };
 
+const contentClasses = `tablet:border-l tablet:p-6`;
+
 const IPInfoCard: FC<IPInfoCardProps> = ({ ipAddress, location, timezone, isp }) => {
   return (
-    <div className="bg-white grid tablet:grid-cols-4 gap-6 rounded-lg tablet:text-left text-center shadow-xl p-4 max-w-[1110px] max-h-[296px] mx-auto">
-      <div className="tablet:border-r">
+    <div className="bg-white grid tablet:grid-cols-4 gap-6 rounded-lg place-items-center tablet:text-left text-center shadow-xl p-4 tablet:max-w-[1110px] max-h-[296px] tablet:h-[161px]">
+      <div className="">
         <h2 className="text-sm font-bold text-gray-500">IP ADDRESS</h2>
         <p className="text-lg font-semibold text-gray-900">{ipAddress}</p>
       </div>
-      <div className="tablet:border-r">
+      <div className={`${contentClasses}`}>
         <h2 className="text-sm font-bold text-gray-500">LOCATION</h2>
         <p className="text-lg font-semibold text-gray-900">{location}</p>
       </div>
-      <div className="tablet:border-r">
+      <div className={`${contentClasses}`}>
         <h2 className="text-sm font-bold text-gray-500">TIMEZONE</h2>
         <p className="text-lg font-semibold text-gray-900">{timezone}</p>
       </div>
-      <div className="">
+      <div className={`${contentClasses}`}>
         <h2 className="text-sm font-bold text-gray-500">ISP</h2>
         <p className="text-lg font-semibold text-gray-900">{isp}</p>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,14 +36,12 @@ const Home = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      {/* Mobile BG */}
-      <div className="">
-        <div className="-z-10 w-full tablet:hidden">
+      {/* MOBILE */}
+      <div className="tablet:hidden">
+        <div className="-z-10 w-full max-h-[300px]">
           <Image src={mobileBg} alt={""} layout="responsive" className="object-cover" />
         </div>
-        <div className=" hidden tablet:block ">
-          <Image src={desktopBg} alt={""} layout="responsive" className="object-cover" />
-        </div>
+
         <div className="mt-6 absolute left-[50%] -translate-x-[50%] z-20 top-0 grid gap-6 w-full container text-center">
           <Heading title={"IP Address Tracker"} />
           <IPInput
@@ -53,6 +51,30 @@ const Home = () => {
             }}
           />
           <div className="">
+            <IPInfoCard
+              ipAddress={"192.212.174.101"}
+              location={"Brooklyn, NY 10001"}
+              timezone={"UTC -05:00"}
+              isp={"SpaceX Starlink"}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* DESKTOP */}
+      <div className="tablet:flex hidden">
+        <div className=" max-h-[280px] w-full">
+          <Image src={desktopBg} alt={""} layout="responsive" className="object-cover" />
+        </div>
+        <div className="mt-6  absolute left-[50%] -translate-x-[50%] z-20 top-0 grid gap-6 w-full container text-center">
+          <Heading title={"IP Address Tracker"} />
+          <IPInput
+            initialValue={""}
+            onButtonClick={() => {
+              console.log("SEARCHED");
+            }}
+          />
+          <div className="mx-auto">
             <IPInfoCard
               ipAddress={"192.212.174.101"}
               location={"Brooklyn, NY 10001"}
